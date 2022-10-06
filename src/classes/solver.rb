32c6325948fb57b342610af395931e9e169cc968
usbbush <jeanbienvenusb@gmail.com>
2022-10-06 16:34:03 +0200

    Implement factorial method

--- src/classes/solver.rb
+++ src/classes/solver.rb
@@ -1,7 +1,9 @@
 class Solver
   class << self
     def factorial(num)
-
+      raise 'Number is negative' if num < 0
+      return 1 unless num.positive?
+      num * factorial(num - 1)
     end
     def reverse(word); end
     def fizzbuzz(num); end
